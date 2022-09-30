87554a0d7436f6506935e958b01dd9b000e9d852
luisarevalo21 <arevaloluis89@yahoo.com>
2022-09-29 20:42:01 -0700

    adjusted

--- db/index.js
+++ db/index.js
@@ -1,11 +1,18 @@
 const Pool = require("pg").Pool;
 
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   localhost: process.env.DB_HOST,
+//   database: "pokemon",
+//   password: "",
+//   port: process.env.DB_PORT,
+// });
 const pool = new Pool({
-  user: process.env.DB_USER,
-  localhost: process.env.DB_HOST,
-  database: "pokemon",
-  password: "",
-  port: process.env.DB_PORT,
+  user: process.env.PG_USER,
+  host: process.env.PG_ENDPOINT,
+  database: process.env.PG_DB,
+  password: process.env.PG_PASS,
+  port: process.env.PG_PORT,
 });
 
 module.exports = pool;
