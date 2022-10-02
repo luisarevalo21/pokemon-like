49536b7ffb5bafb3faadebf2b4f199c9f77432b4
luisarevalo21 <arevaloluis89@yahoo.com>
2022-10-01 21:43:16 -0700

    testing

--- db/index.js
+++ db/index.js
@@ -3,8 +3,8 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   user: process.env.DB_USER,
   localhost: process.env.DB_HOST,
-  database: "pokemon",
-  password: "",
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 });
 
